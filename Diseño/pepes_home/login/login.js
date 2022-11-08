4ff6f5d773b22c21b1d9048da1a4dd4c47e7c22f
diegocaserosmr@gmail.com <99729923+imcasero@users.noreply.github.com>
2022-11-08 18:44:34 +0100

    Intento mostrar teclado contraseña 08-11-2022

--- Diseño/pepes_home/login/login.js
+++ Diseño/pepes_home/login/login.js
@@ -1,4 +1,7 @@
 let clickPass = document.getElementById("passInput");
+let tabla_pass = document.getElementById("div-tabla");
+const tabla = document.createElement('table');
+const tr = document.createElement('tr');
 function validar() {
   let emailValue = document.getElementById("emailInput").value;
   emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
@@ -31,4 +34,16 @@ function tablaRandom() {
   for (let index = 0; index < array.length; index++) {
     console.log(array[index]);
   }
+  
+  tabla.setAttribute("id" , "tablaDatos");
+  
+  //seleccionar un padre (tabla_pass)
+  //agregamos el nodo
+  tabla_pass.appendChild(tabla);
+  const tablaDatos = document.getElementById("tablaDatos");
+  for (let i = 0; i < 2; i++) {//porque me crea un solo td???
+    tablaDatos.appendChild(tr);
+    
+  }
 }
+
