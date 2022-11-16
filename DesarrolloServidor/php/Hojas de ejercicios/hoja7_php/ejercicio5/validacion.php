1d68f6d0eae2ccf3ff5bfb197a009b7c75f9ad65
imcasero <99729923+imcasero@users.noreply.github.com>
2022-11-16 10:22:50 +0100

    hoja 5 de php , terminada con bug(repinta tabla si no existe)

--- DesarrolloServidor/php/Hojas de ejercicios/hoja7_php/ejercicio5/validacion.php
+++ DesarrolloServidor/php/Hojas de ejercicios/hoja7_php/ejercicio5/validacion.php
@@ -3,6 +3,46 @@ $nombre;
 $destino;
 $duracion;
 $salida;
+$documento = '<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    
+    <form action="validacion.php" enctype="multipart/form-data" method="post">
+        <table border="1" cellspacing="0">
+            <tr>
+                <td><label for="nombre">Introduzca el nombre del circuito</label></td>
+                <td><input type="text" name="nombre"></td>
+            </tr>
+            <tr>
+                <td><label for="destino">Introduzca el destino</label></td>
+                <td><input type="text" name="destino"></td>
+            </tr>
+            <tr>
+                <td><label for="duracion">Introduzca la duracion</label></td>
+                <td><input type="text" name="duracion"></td>
+            </tr>
+            <tr>
+                <td><label for="salida">Introduzca los dias de salida</label></td>
+                <td><input type="text" name="salida"></td>
+            </tr>
+            <tr>
+                <td colspan="2"><button type="submit">Enviar</button></td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>';
+$cadena_error_existe =
+'<p style="color : red; font-size : 20px ;text-align:center"> 
+   El destino ya existe
+</p>';
 $cadena_error = 
 '<p style="color : red; font-size : 20px ;text-align:center"> 
    Todos los campos son obligarotios 
@@ -77,16 +117,152 @@ $inter = array();
         </html>
         ';
     }else{
-        $existe;
+        $cadena = "$nombre:$destino:$duracion:$salida\n";
+        // echo $cadena;
+        $existe = false;
         $fichero_lec = @fopen('index.txt' , 'r');
-        
+        if (!$fichero_lec) {
+            die('Problemas con el fichero lectura');
+        }
         while (!feof($fichero_lec)) {
-            $lec = fgets($fichero);
+            $lec = fgets($fichero_lec);
             if (strpos($lec , $destino)) {
-                echo 'el destino introducio existe';
+                $existe = true;
+                break;
             }
         }
-
-        $cadena = "$nombre:$destino:$duracion:$salida";
+        if (!$existe) {
+            echo 'Insertando datos...';
+            $fichero_es = @fopen('index.txt', 'a+');
+            if (!$fichero_es) {
+                die('Problemas con el fichero escritura');
+            }
+            fwrite($fichero_es , $cadena);
+            @fclose($fichero_es);
+        }
     }
+    @fclose($fichero_lec);
+    
+    if ($existe) {
+        echo $documento;
+        echo $cadena_error_existe;
+        //generar tabla
+    $array_datos_aux = array();
+    $datos_fichero = @fopen('index.txt' , 'r+');
+    if (!$datos_fichero) {
+        die('Probelmas con el fichero datos');
+    }
+    while (!feof($datos_fichero)) {
+        $fila = fgets($datos_fichero);
+        array_push($array_datos_aux , $fila);
+    }
+    $array_datos = array();
+    foreach ($array_datos_aux as $string_datos) {
+        $a = explode(':' , $string_datos); 
+        array_push($array_datos , $a);
+    }
+    
+
+    //pintar tabla
+
+    echo '<table border="1" cellspacing="0">';
+    foreach ($array_datos as $clave => $array) {
+        echo '<tr>';
+        foreach ($array as $valor) {
+            echo '<td>'.$valor.'</td>';
+        }
+        echo '</tr>';
+    }
+    echo '</table>';
+
+    }else {
+        echo $documento;
+        $array_datos_aux = array();
+        $datos_fichero = @fopen('index.txt' , 'r+');
+        if (!$datos_fichero) {
+            die('Probelmas con el fichero datos');
+        }
+        while (!feof($datos_fichero)) {
+            $fila = fgets($datos_fichero);
+            array_push($array_datos_aux , $fila);
+        }
+        $array_datos = array();
+        foreach ($array_datos_aux as $string_datos) {
+            $a = explode(':' , $string_datos); 
+            array_push($array_datos , $a);
+            //generar tabla
+    $array_datos_aux = array();
+    $datos_fichero = @fopen('index.txt' , 'r+');
+    if (!$datos_fichero) {
+        die('Probelmas con el fichero datos');
+    }
+    while (!feof($datos_fichero)) {
+        $fila = fgets($datos_fichero);
+        array_push($array_datos_aux , $fila);
+    }
+    $array_datos = array();
+    foreach ($array_datos_aux as $string_datos) {
+        $a = explode(':' , $string_datos); 
+        array_push($array_datos , $a);
+    }
+    
+
+    //pintar tabla
+
+    echo '<table border="1" cellspacing="0">';
+    foreach ($array_datos as $clave => $array) {
+        echo '<tr>';
+        foreach ($array as $valor) {
+            echo '<td>'.$valor.'</td>';
+        }
+        echo '</tr>';
+    }
+    echo '</table>';
+
+        }
+        
+    
+        //pintar tabla
+    
+        echo '<table border="1" cellspacing="0">';
+        foreach ($array_datos as $clave => $array) {
+            echo '<tr>';
+            foreach ($array as $valor) {
+                echo '<td>'.$valor.'</td>';
+            }
+            echo '</tr>';
+        }
+        echo '</table>';
+      
+    }
+    
+    //generar tabla
+    $array_datos_aux = array();
+    $datos_fichero = @fopen('index.txt' , 'r+');
+    if (!$datos_fichero) {
+        die('Probelmas con el fichero datos');
+    }
+    while (!feof($datos_fichero)) {
+        $fila = fgets($datos_fichero);
+        array_push($array_datos_aux , $fila);
+    }
+    $array_datos = array();
+    foreach ($array_datos_aux as $string_datos) {
+        $a = explode(':' , $string_datos); 
+        array_push($array_datos , $a);
+    }
+    
+
+    //pintar tabla
+
+    echo '<table border="1" cellspacing="0">';
+    foreach ($array_datos as $clave => $array) {
+        echo '<tr>';
+        foreach ($array as $valor) {
+            echo '<td>'.$valor.'</td>';
+        }
+        echo '</tr>';
+    }
+    echo '</table>';
+
 ?>
