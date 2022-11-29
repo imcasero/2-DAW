5ca8533e67251cec2544aaef346585547c847db3
imcasero <99729923+imcasero@users.noreply.github.com>
2022-11-29 12:39:51 +0100

    ejercicio tabla dinamica DOM iniciado

--- DesarrolloCliente/js/tablaDOM/index.js
+++ DesarrolloCliente/js/tablaDOM/index.js
@@ -1,4 +1,22 @@
-const div_tabla = document.getElementById('div-tabla');
-//creo la tabla dentro del div
-div_tabla.appendChild(table);//Creo la tabla dentro del div
-table.setAttribute("id", "tablaDatos");
+window.onload = init();
+
+
+function init(){
+    
+    const TABLA_GENERAL = document.getElementById('tabla_general');
+    //creo la tabla dentro del div
+    
+    
+    for (let i = 0; i < 8; i++) {
+        var fila = document.createElement('tr'); //Tienen que estar dentro del for, si no soloc reara uno
+        TABLA_GENERAL.appendChild(fila);
+        for (let j = 0; j < 10; j++) {
+            var colum = document.createElement('td');
+            fila.appendChild(colum);
+        }
+    }
+}
+
+
+
+
