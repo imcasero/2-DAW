3b4ff4b18cbc9769841f973a9b428e3d31771651
imcasero <diegocaserosmr@gmail.com>
2022-12-02 22:41:50 +0100

    falta insertar fecha en fichero
    No se pueden insertar objetos en los ficheros , solo cadenas

--- DesarrolloServidor/php/Hojas de ejercicios/movilidad/php/residentes.php
+++ DesarrolloServidor/php/Hojas de ejercicios/movilidad/php/residentes.php
@@ -1,23 +1,8 @@
 <?php
-    $errors = array();
-
-    if (!$_POST['matricula']){
-        $errors['matricula'] = 'La matricula no puede estar en blanco'; 
-    };
-    if (!$_POST['direccion']){
-        $errors['direccion'] = 'La direccion no puede estar en blanco'; 
-    };
-    if (!$_POST['inicio']){
-        $errors['inicio'] = 'La fecha de inicio no puede estar en blanco'; 
-    };
-    if (!$_POST['salida']){
-        $errors['salida'] = 'La fecha de salida no puede estar en blanco'; 
-    };
-
-    if (count($errors) > 0){
-        echo '
-        <!DOCTYPE html>
-        <html lang="en">
+function repintar($cadena){
+            echo '
+            <!DOCTYPE html>
+            <html lang="en">
             <head>
                 <meta charset="UTF-8" />
                 <meta http-equiv="X-UA-Compatible" content="IE=edge" />
@@ -37,14 +22,47 @@
                     <input type="text" name="inicio" placeholder="00/00/0000"></br></br>
 
                     <label for="salida">Fecha de salida</label></br>
-                    <input type="text" name="salida" placeholder="00/00/0000"></br></br>
+                    <input type="text" name="salida" placeholder="00/00/0000"></br>
+                    ' .$cadena. '
 
                     <button type="submit">Enviar</button>
                 </form>
             </body>
-        </html>
-        ';
+            </html>
+            ';
+        }
+    function insertar($matricula , $direccion , $inicio , $salida){
+        $cad = "\n" .$matricula . ' ' . $direccion . ' ' . $inicio . ' ' . $salida;
+
+        $fichero = @fopen('../txt/residentesYHoteles.txt' , 'a+');
+        if (!$fichero) {
+            die('fichero inaccesible');
+        };
+        fwrite($fichero , $cad);
+
+        echo 'Solicitud enviada con existo';
+        }
+    $errors = array();
+
+    if (!$_POST['matricula']){
+        $errors['matricula'] = 'La matricula no puede estar en blanco'; 
+    };
+    if (!$_POST['direccion']){
+        $errors['direccion'] = 'La direccion no puede estar en blanco'; 
+    };
+    if (!$_POST['inicio']){
+        $errors['inicio'] = 'La fecha de inicio no puede estar en blanco'; 
+    };
+    if (!$_POST['salida']){
+        $errors['salida'] = 'La fecha de salida no puede estar en blanco'; 
+    };
+
+    if (count($errors) > 0){
         
+        $cad_1 = '<p style="color : red; font-size : 10px "> 
+                        todos los campos son obligatorios
+                    </p>';
+            repintar($cad_1);
     }else {
         $matricula = $_POST['matricula'];
         $direccion = $_POST['direccion'];
@@ -56,23 +74,23 @@
 
         $salida_date = new DateTime($salida);
         
+        if ($inicio_date > $salida_date){
+            $cad_2 = '<p style="color : red; font-size : 10px "> 
+                        La fecha de inicio es mayor ue la fecha de salida
+                    </p>';
+            repintar($cad_2);
+        } else {
+            insertar($matricula , $direccion , $inicio_date , $salida_date);
+        }
+        
+
         
         
 
-        echo $inicio_date->format("Y/m/d");
-        echo $salida_date->format("Y/m/d");
-
-
         
         
         
 
-        // $cad = "\n" .$matricula . ' ' . $direccion . ' ' . $inicio . ' ' . $salida;
-
-        // $fichero = @fopen('../txt/residentesYHoteles.txt' , 'a+');
-        // if (!$fichero) {
-        //     die('fichero inaccesible');
-        // };
-        // fwrite($fichero , $cad);
+        
     }
 ?>
