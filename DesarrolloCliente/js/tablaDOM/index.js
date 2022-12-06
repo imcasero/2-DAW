c7eafe667a1e2e750121660674ca4c7d87f7b48c
imcasero <diegocaserosmr@gmail.com>
2022-12-06 11:33:04 +0100

    tabla ramdom DOM, sin elementos ramdom

--- DesarrolloCliente/js/tablaDOM/index.js
+++ DesarrolloCliente/js/tablaDOM/index.js
@@ -7,18 +7,8 @@ function init() {
     var fila = document.createElement("tr"); //Tienen que estar dentro del for, si no solo creara uno
     TABLA_GENERAL.appendChild(fila);
     for (let j = 0; j < 10; j++) {
-      var colum = document.createElement("td");//creo td
-      var rad = document.createElement("INPUT");
-      rad.setAttribute("type", "radio");//input radio
-      var sub = document.createElement("INPUT");
-      sub.setAttribute("type", "submit");//input submit
-      var check = document.createElement("INPUT");
-      check.setAttribute("type", "checkbox");//input checkbox
-      var elementos = new Array(" ", rad, sub, check); //array de elementos
-      fila.appendChild(colum);//columna hija de fila
-      colum.appendChild(
-        elementos[Math.floor(Math.random() * elementos.length)] //elementos hijos de colum
-      );
+      var colum = document.createElement("td"); //creo td
+      fila.appendChild(colum); //columna hija de fila
     }
   }
 }
