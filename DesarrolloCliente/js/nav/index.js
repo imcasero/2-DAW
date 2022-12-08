0df64aa8934ca39ea878d88a2c8227e47682b54b
imcasero <diegocaserosmr@gmail.com>
2022-12-08 18:50:35 +0100

    Avance en raiz de js

--- DesarrolloCliente/js/nav/index.js
+++ DesarrolloCliente/js/nav/index.js
@@ -1,3 +1,27 @@
 const lista = document.getElementById("lista");
-
-addEventListener();
+let hijo;
+let i = 0;
+let j = 0;
+lista.addEventListener("click", (e) => {
+  let uls = document.createElement("ul");
+  uls.setAttribute("id", `nodohijo${i}`);
+  e.currentTarget.appendChild(uls);
+  hijo = document.getElementById(`nodohijo${i}`);
+  añadir(hijo);
+});
+function añadir() {
+  if (!hijo) {
+    let lis = document.createElement("li");
+    i++;
+    lis.textContent = `nodo0${i}`;
+    lista.appendChild(lis);
+  } else {
+    let lis = document.createElement("li");
+    i++;
+    lis.textContent = `nodo0${j}${i}`;
+    hijo.appendChild(lis);
+  }
+}
+function borrar() {
+  console.log("borrar");
+}
