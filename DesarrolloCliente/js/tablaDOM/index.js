4d0b8cfcda93f36a813262a58c789ec64c2fd446
imcasero <99729923+imcasero@users.noreply.github.com>
2022-12-12 09:14:42 +0100

    Tabla DOM terminada
    Tener en cuenta que no se usa innerHTML, si no el appendChild, y creo cada elemento una y otra vez dentro del for

--- DesarrolloCliente/js/tablaDOM/index.js
+++ DesarrolloCliente/js/tablaDOM/index.js
@@ -1,22 +1,22 @@
 window.onload = init();
 
 function init() {
-  let radio = crearInput("radio");
-  let sub = crearInput("submit");
-  let check = crearInput("check");
-  let array = new Array(radio, sub, check);
-  let long = array.length;
-
   const TABLA_GENERAL = document.getElementById("tabla_general");
   //creo la tabla dentro del div
   for (let i = 0; i < 8; i++) {
     let fila = document.createElement("tr"); //Tienen que estar dentro del for, si no solo creara uno
     TABLA_GENERAL.appendChild(fila);
     for (let j = 0; j < 10; j++) {
+      let radio = crearInput("radio");
+      let sub = crearInput("submit");
+      let check = crearInput("checkbox");
+      let array = new Array(radio, sub, check);
+      let long = array.length;
+
       let num = Math.floor(Math.random() * long);
       let colum = document.createElement("td"); //creo td
       fila.appendChild(colum); //columna hija de fila
-      colum.innerHTML = array[num];
+      colum.appendChild(array[num]);
     }
   }
 }
