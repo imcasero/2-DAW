791c77bbc967f853510a310aad92b02686849b27
imcasero <99729923+imcasero@users.noreply.github.com>
2022-12-16 14:10:49 +0100

    updates de hoja13

--- DesarrolloServidor/php/Hojas de ejercicios/hoja13_php/añadir_cookie.php
+++ DesarrolloServidor/php/Hojas de ejercicios/hoja13_php/añadir_cookie.php
@@ -1,4 +1,6 @@
 <?php
+
+echo $_POST['nivel'];
     $cadena_error = 
     '<p style="color : red; font-size : 10px ">Todos los campos son obligatorios</p>';
     $errors = array();
@@ -10,42 +12,43 @@
     }
     if (empty($_POST['nivel'])){
         array_push($errors , false);
+        
     }
     
     if(in_array(false , $errors)){
-        echo '
-        <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8" />
-                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <title>Cookies</title>
-            </head>
-            <body>
-                <h3>Lista de cookies</h3>
-                <form action="añadir_cookie.php" method="post">
-                <label for="nombre">Nombre de la cookie</label>
-                <input type="text" name="nombre" >
+        // echo '
+        // <!DOCTYPE html>
+        //     <html lang="en">
+        //     <head>
+        //         <meta charset="UTF-8" />
+        //         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        //         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        //         <title>Cookies</title>
+        //     </head>
+        //     <body>
+        //         <h3>Lista de cookies</h3>
+        //         <form action="añadir_cookie.php" method="post">
+        //         <label for="nombre">Nombre de la cookie</label>
+        //         <input type="text" name="nombre" >
 
-                <label for="contenido">Introduzca el contenido de la cookie</label>
-                <input type="text" name="contenido"></br></br>
-                <label for="nivel">Nivel donde crear</label>
-                <select name="nivel">
-                    <option value="0">Nivel 0</option>
-                    <option value="1">Nivel 1</option>
-                    <option value="2">Nivel 2</option>
-                </select></br>
-                '.$cadena_error.'
-                </br>
-                <button type="submit">Crear cookie</button>
-                </form>
-                <h3>Listar cookies</h3>
-                <p>Nivel 0 --> <a href="nivel0.php">Nivel 0</a></p> 
-                <p>Nivel 0 --> Nivel 1 --> <a href="nivel1.php">Nivel 1</a></p> 
-                <p>Nivel 0 --> Nivel 1 --> Nivel 2 --> <a href="nivel2.php">Nivel 2</a></p> 
-            </body>
-        </html>';
+        //         <label for="contenido">Introduzca el contenido de la cookie</label>
+        //         <input type="text" name="contenido"></br></br>
+        //         <label for="nivel">Nivel donde crear</label>
+        //         <select name="nivel">
+        //             <option value="0">Nivel 0</option>
+        //             <option value="1">Nivel 1</option>
+        //             <option value="2">Nivel 2</option>
+        //         </select></br>
+        //         '.$cadena_error.'
+        //         </br>
+        //         <button type="submit">Crear cookie</button>
+        //         </form>
+        //         <h3>Listar cookies</h3>
+        //         <p>Nivel 0 --> <a href="nivel0.php">Nivel 0</a></p> 
+        //         <p>Nivel 0 --> Nivel 1 --> <a href="nivel1.php">Nivel 1</a></p> 
+        //         <p>Nivel 0 --> Nivel 1 --> Nivel 2 --> <a href="nivel2.php">Nivel 2</a></p> 
+        //     </body>
+        // </html>';
     } else {
         $nombre = $_POST['nombre'];
         $contenido = $_POST['contenido'];
---
+++ DesarrolloServidor/php/Hojas de ejercicios/hoja13_php/nivel1/nivel2/nivel2.php
@@ -0,0 +1,5 @@
+<?php
+ foreach($_COOKIE as $key => $val){
+    print_r($val); echo '<br>';
+ }
+?>
