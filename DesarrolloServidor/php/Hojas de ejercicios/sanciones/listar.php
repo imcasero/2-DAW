3c18c618579eb6c157c8b141e9cc90505bdff68c
imcasero <diegocaserosmr@gmail.com>
2023-01-11 18:38:52 +0100

    Listado sin estilos

--- DesarrolloServidor/php/Hojas de ejercicios/sanciones/listar.php
+++ DesarrolloServidor/php/Hojas de ejercicios/sanciones/listar.php
@@ -1,3 +1,33 @@
 <?php
-    
+
+    echo '
+    <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+    <body>';
+
+    $fichero = @fopen("alumnos_sancionados.txt", "r");
+    if (!$fichero) {
+        die("Error en el fichero");
+    }
+    echo '<div class="contenedor">';
+    echo '<table>';
+    while(!feof($fichero)){
+        $fila = explode("," , fgets($fichero));
+        echo '<tr>';
+        foreach ($fila as $key => $value) {
+            echo '<td>' . $value . '</td>';
+        }
+        echo '</tr>';
+    }
+    echo '</table>';
+    echo '</div>';
+
+    echo '</body>
+</html>';
 ?>
